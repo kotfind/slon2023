d51b9f5d722a81c36592a3487cbd5253df73a72c
kotfind <kotfind@yandex.ru>
2023-04-13 17:36:44 +0300

    Add some output

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 #include <iostream>
 
 int main() {
+    std::cout << "Polish:\n";
     std::string s;
     std::getline(std::cin, s);
 
     MathExpr expr(s);
+    std::cout << "\nTree:\n";
     expr.printAsTree(std::cout);
+
+    std::cout << "\nAnswer:\n";
     std::cout << expr << " = " << expr.eval() << '\n';
 }
